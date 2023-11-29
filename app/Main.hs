dc3bb1dec7cbdc849b6d04d87328034ce96b5ba0
KrunchyKrisp <m.damidavicius@gmail.com>
2023-11-29 12:57:26 +0200

    1. depth, occurs, mapGTree

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,20 @@ main = do
 --1
 data GTree a = Leaf a | Gnode [GTree a]
 
+depth :: GTree a -> Int
+depth (Leaf _) = 1
+depth (Gnode []) = 1
+depth (Gnode xs) = 1 + maximum (map depth xs)
+
+occurs :: Eq a => GTree a -> a -> Bool
+occurs (Leaf a) x = a == x
+occurs (Gnode []) _ = False
+occurs (Gnode xs) x = any (`occurs` x) xs
+
+mapGTree :: (a -> b) -> GTree a -> GTree b
+mapGTree f (Leaf a)  = Leaf (f a)
+mapGTree _ (Gnode []) = Gnode []
+mapGTree f (Gnode xs)  = Gnode (map (mapGTree f) xs)
 
 --2
 
